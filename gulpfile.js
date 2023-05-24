787d4536d7724995b71ccbd1db5622a1c535f598
Nguyen Pham <admin@madelab.dev>
2023-05-24 14:04:54 -0500

    sfe

--- gulpfile.js
+++ gulpfile.js
@@ -85,7 +85,7 @@ gulp.task('css:compile', function () {
 });
 
 // Minify CSS
-gulp.task('css:minify', ['css:compile'], function () {
+gulp.task('css:minify', gulp.series('css:compile'), function () {
   return gulp.src([
     './css/*.css',
     '!./css/*.min.css'
@@ -104,7 +104,7 @@ gulp.task('css:minify', ['css:compile'], function () {
 });
 
 // CSS
-gulp.task('css', ['css:compile', 'css:minify']);
+gulp.task('css', gulp.series('css:compile', 'css:minify'));
 
 // Minify JavaScript
 gulp.task('js:minify', function () {
@@ -127,10 +127,10 @@ gulp.task('html', function () {
 });
 
 // JS
-gulp.task('js', ['js:minify']);
+gulp.task('js', gulp.series('js:minify'));
 
 // Default task
-gulp.task('default', ['css', 'js', 'vendor', 'html']);
+gulp.task('default', gulp.series('css', 'js', 'vendor', 'html'));
 
 // Configure the browserSync task
 gulp.task('browserSync', function () {
@@ -142,8 +142,8 @@ gulp.task('browserSync', function () {
 });
 
 // Dev task
-gulp.task('dev', ['css', 'js', 'browserSync'], function () {
-  gulp.watch('./scss/*.scss', ['css']);
-  gulp.watch('./js/*.js', ['js']);
+gulp.task('dev', gulp.series('css', 'js', 'browserSync'), function () {
+  gulp.watch('./scss/*.scss', gulp.series('css'));
+  gulp.watch('./js/*.js', gulp.series('js'));
   gulp.watch('./*.html', browserSync.reload);
 });
